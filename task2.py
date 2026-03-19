2c0fbdf53f4293c313742b648a38e8994419a551
Sarchit007 <sarchit971@gmail.com>
2026-03-19 15:30:23 +0530

    commiting new changes

--- task2.py
+++ task2.py
@@ -1,4 +1,8 @@
 # Question2
+from time import sleep
+from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.common.by import By
+
 o = ChromeOptions()
 o.add_experimental_option("detach", True)
 driver = Chrome(options=o)
